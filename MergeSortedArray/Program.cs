21e417cd354c67d61153170f2b0208093c5c360a
Alina Popa <alina.popa@maadamian.net>
2023-09-09 08:55:16 +0300

    Added code files.

---
+++ MergeSortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using MergeSortedArray;
+
+// example 1
+Console.WriteLine("Example 1");
+var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+var nums2 = new int[] { 2, 5, 6 };
+Solution.Merge(nums1, 3, nums2, 3);
+Console.WriteLine();
+
+
+// example 2
+Console.WriteLine("Example 2");
+nums1 = new int[] { 1 };
+nums2 = new int[] { };
+Solution.Merge(nums1, 1, nums2, 0);
+Console.WriteLine();
+
+// example 3
+Console.WriteLine("Example 3");
+nums1 = new int[] { 0 };
+nums2 = new int[] { 1 };
+Solution.Merge(nums1, 0, nums2, 1);
+Console.WriteLine();
---
+++ MergeSortedArray/Solution.cs
@@ -0,0 +1,56 @@
+﻿using Console = System.Console;
+
+namespace MergeSortedArray
+{
+    internal static class Solution
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var firstArray = new int[m];
+            Array.Copy(nums1, firstArray, m);
+            Console.WriteLine($"first array is {string.Join(", ", firstArray)}");
+            var temp = MergeTwoArrays(firstArray, nums2);
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                nums1[i] = temp[i];
+            }
+        }
+
+        private static int[] MergeTwoArrays(int[] a, int[] b)
+        {
+            var merged = new int[a.Length + b.Length];
+            var i = 0;
+            var j = 0;
+            var k = 0;
+
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    Console.WriteLine($"Adding {a[i]} to merged.");
+                    merged[k++] = a[i++];
+                }
+                else
+                {
+                    Console.WriteLine($"Adding {b[j]} to merged.");
+                    merged[k++] = b[j++];
+                }
+            }
+
+            while (i < a.Length)
+            {
+                Console.WriteLine($"Adding {a[i]} to merged.");
+                merged[k++] = a[i++];
+            }
+
+            while (j < b.Length)
+            {
+                Console.WriteLine($"Adding {b[j]} to merged.");
+                merged[k++] = b[j++];
+            }
+
+            return merged;
+        }
+    }
+}
